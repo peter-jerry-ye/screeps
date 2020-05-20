80ae2010065ba6594adc3f937d865cd2c7022779
YE Zihang <jerryzihye@outlook.com>
2020-05-21 00:31:12 +0200

    (b) fix path search

--- src/spawn.js
+++ src/spawn.js
@@ -4,7 +4,7 @@ module.exports.job = function(spawn) {
 		spawn.memory.source = spawn.pos.findClosestByRange(FIND_SOURCES).id;
 	}
 	if (spawn.memory.path == null) {
-		spawn.memory.path = spawn.pos.findPathTo(spawn.memory.source, {serialize: true, ignoreCreeps: true})
+		spawn.memory.path = spawn.pos.findPathTo(Game.getObjectById(spawn.memory.source), {serialize: true, ignoreCreeps: true})
 		Room.deserializePath(spawn.memory.path)
 			.map(pos => spawn.room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD));
 	}
