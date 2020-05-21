f1d701205c6f25e0b324075abd6aa60c5ccf486f
YE Zihang <jerryzihye@outlook.com>
2020-05-21 18:32:53 +0200

    (b) fix a maintainer always repair a wall

--- src/maintainer.js
+++ src/maintainer.js
@@ -1,15 +1,28 @@
 module.exports.job = (creep) => {
 	if (creep.memory.target == null 
 	        || Game.getObjectById(creep.memory.target) == null
+	        || (Game.getObjectById(creep.memory.target).structureType == STRUCTURE_WALL
+	                && Game.getObjectById(creep.memory.target).hits > creep.memory.previousHits + 1000)
 	        || Game.getObjectById(creep.memory.target).hits == Game.getObjectById(creep.memory.target).hitsMax) {
-		let structures = creep.room.find(FIND_STRUCTURES, {filter: object => object.hits < object.hitsMax}).sort((a, b) => a.hits - b.hits);
+		let structures = creep.room.find(FIND_STRUCTURES, {filter: object => object.structureType != STRUCTURE_WALL && object.hits < object.hitsMax}).sort((a, b) => a.hits - b.hits);
 		if (structures.length === 0)
-		    return;
-		
-		creep.memory.target = structures[0].id;
-		creep.memory.source = structures[0].pos.findClosestByPath(FIND_SOURCES, {
-		    ignoreCreeps: true
-		}).id;
+		{
+		    let walls = creep.room.find(FIND_STRUCTURES, {filter: object => object.structureType == STRUCTURE_WALL && object.hits < object.hitsMax}).sort((a, b) => a.hits - b.hits);
+		    if (walls.length === 0) {
+		        return;
+		    }
+		    creep.memory.target = walls[0].id;
+		    creep.memory.previousHits = walls[0].hits;
+		    creep.memory.source = walls[0].pos.findClosestByPath(FIND_SOURCES, {
+    		    ignoreCreeps: true
+		    }).id;
+		}
+		else {
+		    creep.memory.target = structures[0].id;
+		    creep.memory.source = structures[0].pos.findClosestByPath(FIND_SOURCES, {
+    		    ignoreCreeps: true
+		    }).id;
+		}
 	}
 	let {target, source} = creep.memory; // id
 	source = Game.getObjectById(source);
