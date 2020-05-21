4bda31ff02acb0760b7fd5a9b45e7d5ab2546565
YE Zihang <jerryzihye@outlook.com>
2020-05-21 17:30:01 +0200

    (m) remove log

--- src/builder.js
+++ src/builder.js
@@ -4,7 +4,6 @@ module.exports.job = (creep) => {
 		if (constructionSites.length === 0)
 		    return;
 		
-		console.log("here");
 		creep.memory.target = constructionSites[0].id;
 		creep.memory.source = constructionSites[0].pos.findClosestByPath(FIND_SOURCES, {
 		    ignoreCreeps: true
@@ -15,15 +14,12 @@ module.exports.job = (creep) => {
 	target = Game.getObjectById(target);
 	
 	if (creep.store[RESOURCE_ENERGY] == 0 && creep.harvest(source) == ERR_NOT_IN_RANGE) {
-	    console.log(1);
 	    creep.moveTo(source);
 	}
 	else if (creep.store[RESOURCE_ENERGY] === creep.store.getCapacity() && creep.build(target) == ERR_NOT_IN_RANGE) {
-	    console.log(2);
 	    creep.moveTo(target);
 	}
 	else {
-	    console.log(3);
 	    creep.harvest(source) === 0 || creep.build(target);
 	}
 };
